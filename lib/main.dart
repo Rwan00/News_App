0ed301381412a164c37498027cd52b5d13347a76
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2023-11-20 22:20:10 +0200

    add DioHelper

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/bloc_observer.dart';
 import 'package:news_app/layouts/news_layout.dart';
+import 'package:news_app/network/dio_helper.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
+  DioHelper.init();
   runApp(const MyApp());
 }
 
@@ -15,29 +20,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         appBarTheme: const AppBarTheme(
-          systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.white,
-            statusBarIconBrightness: Brightness.dark
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.black
-          )
-        ),
+            systemOverlayStyle: SystemUiOverlayStyle(
+                statusBarColor: Colors.white,
+                statusBarIconBrightness: Brightness.dark),
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            iconTheme: IconThemeData(color: Colors.black)),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: Colors.orangeAccent,
-          elevation: 20
-        ),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.orangeAccent,
+            elevation: 20),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      themeMode: ThemeMode.light,
       home: const NewsLayout(),
     );
   }
